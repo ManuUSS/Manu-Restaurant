572e62869d44e9f22d4208e87701edf86821bd88
Manuel Ulate <fr3zs09@gmail.com>
2023-01-21 12:39:20 -0600

    hompeage temporal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-import { Typography } from '@mui/material'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 import { ShopLayout } from "components/layout";
+import { initialData } from 'database/products';
+
 
 export default function Home() {
   return (
@@ -7,6 +9,28 @@ export default function Home() {
       <>
         <Typography variant='h1' component='h1'>Tienda</Typography>
         <Typography variant='h2' component='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+        
+        <Grid container spacing={4}>
+
+          {
+            initialData.products.map( (product) => (
+              <Grid  item xs={6} sm={4} key={ product.slug }>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia 
+                      component='img'
+                      image={`products/${ product.images[0] }`}
+                      alt={ product.title }
+                    />
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))
+          }
+
+
+
+        </Grid>
       </>
     </ShopLayout>
   )
