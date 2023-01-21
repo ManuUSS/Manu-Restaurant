364db952cc6078781ccdc76737595c7d52ee7f2f
Manuel Ulate <fr3zs09@gmail.com>
2023-01-21 12:09:22 -0600

    estructura del proyecto y layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
+import { Typography } from '@mui/material'
+import { ShopLayout } from "components/layout";
 
 export default function Home() {
   return (
-    <h1>Manu-Shop</h1>
+    <ShopLayout title='Manu - Shop' pageDescription="Encuentra los mejores productos de Tesla aquí">
+      <>
+        <Typography variant='h1' component='h1'>Tienda</Typography>
+        <Typography variant='h2' component='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+      </>
+    </ShopLayout>
   )
 }
