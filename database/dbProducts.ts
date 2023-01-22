b2d6c4cec8c4037a9f8a8acc5bf0fd282cf80edd
Manuel Ulate <fr3zs09@gmail.com>
2023-01-22 15:49:50 -0600

    búsqueda mediante ssr

--- database/dbProducts.ts
+++ database/dbProducts.ts
@@ -23,3 +23,19 @@ export const getAllProductsSlug = async (): Promise<ProductSlug[]> => {
     await db.disconnect();
     return slugs;
 } 
+
+export const getProductsByTerm = async ( term: string ): Promise<IProduct[]> => {
+    
+    term = term.toString().toLowerCase();
+
+    await db.connect();
+
+    const products = await Product.find({ 
+        $text: { $search: term } 
+    }).select('title images price inStock slug -_id')
+    .lean();
+
+    await db.disconnect();
+
+    return products;
+}
--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,24 +1,48 @@
+import { GetServerSideProps, NextPage } from 'next';
 import { Typography } from "@mui/material";
 import { ShopLayout } from "components/layout";
 import { ProductList } from "components/products";
-import { Loader } from "components/ui";
-import { useProducts } from "hooks";
+import { dbProducts } from 'database';
+import { IProduct } from '../../interfaces/products';
 
-const SearchPage = () => {
-    const { products, isLoading } = useProducts('/products');
+interface Props {
+    products: IProduct[] 
+}
+
+const SearchPage:NextPage<Props> = ({ products }) => {
     return (
       <ShopLayout title='Manu - Shop Búsqueda' pageDescription="Encuentra los mejores productos de Tesla aquí">
         <>
           <Typography variant='h1' component='h1'>Buscar productos</Typography>
           <Typography fontSize={ 14 } sx={{ mb: 1 }} component='h2'>Todos los productos</Typography>
           
-          {
-            isLoading ? <Loader /> : <ProductList  products={ products } />
-          }
+           <ProductList  products={ products } />
   
         </>
       </ShopLayout>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    
+    const { query = '' } = params as { query: string };  
+
+    if ( query.length === 0 ) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: 'false'
+            }
+        }
+    }
+
+    let products = await dbProducts.getProductsByTerm( query );
+
+    return {
+        props: {
+            products
+        }
+    }
 }
 
 export default SearchPage
