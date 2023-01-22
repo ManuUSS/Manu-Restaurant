c541d3b5435e8c7469c127fcdfae13bc639ac167
Manuel Ulate <fr3zs09@gmail.com>
2023-01-22 14:32:06 -0600

    UI Context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { SWRConfig } from 'swr';
 import { lightTheme } from 'themes';
+import { UIProvider } from 'context';
 
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -12,10 +13,12 @@ export default function App({ Component, pageProps }: AppProps) {
         fetcher: ( resource, init ) => fetch( resource, init ).then( res => res.json() )
       }}
     >
-      <ThemeProvider theme={ lightTheme }>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <UIProvider>
+        <ThemeProvider theme={ lightTheme }>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UIProvider>
     </SWRConfig>
   ) 
 }
