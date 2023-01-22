1994467d659a2e13282e44c8ca92c3beac4662fe
Manuel Ulate <fr3zs09@gmail.com>
2023-01-22 14:42:40 -0600

    funcionalidad del sidemenu con navegacion y toggle

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,10 +1,13 @@
-import { useMemo } from 'react';
+import { useContext, useMemo } from 'react';
 import { useRouter } from 'next/router';
 import NextLink from 'next/link';
 import { AppBar, Badge, Box, Button, IconButton, Link, Toolbar, Typography } from '@mui/material';
 import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
+import { UIContext } from 'context';
 
 export const Navbar = () => {
+
+    const { toggleSideMenu } = useContext( UIContext );
 
     const { pathname } = useRouter(); 
 
@@ -56,7 +59,7 @@ export const Navbar = () => {
                 </Link>
             </NextLink>
 
-            <Button>
+            <Button onClick={ toggleSideMenu }>
                 Menú
             </Button>
         
--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -1,12 +1,25 @@
 import { SearchOutlined, AccountCircleOutlined, ConfirmationNumberOutlined, MaleOutlined, FemaleOutlined, EscalatorWarningOutlined, VpnKeyOutlined, LoginOutlined, CategoryOutlined, AdminPanelSettings } from "@mui/icons-material"
-import { Box, Divider, Drawer, IconButton, Input, InputAdornment, List, ListItem, ListItemIcon, ListItemText, ListSubheader } from "@mui/material"
+import { Box, Divider, Drawer, IconButton, Input, InputAdornment, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader } from "@mui/material"
+import { useContext } from "react"
+import { UIContext } from '../../context/ui/UIContext';
+import { useRouter } from 'next/router';
 
 export const SideMenu = () => {
+
+    const { isMenuOpen, toggleSideMenu } = useContext( UIContext );
+    const router = useRouter();
+
+    const navigateTo = ( url:string ) => {
+        toggleSideMenu();
+        router.push( url );
+    }   
+
   return (
     <Drawer
-        open={ false }
+        open={ isMenuOpen }
         anchor="right"
         sx={{ backdropFilter: 'blur(4px)', transition: 'all .5s ease-out' }}
+        onClose={ toggleSideMenu }
     >
         <Box sx={{ width: 250, paddingTop: 5 }}>
             
@@ -28,81 +41,87 @@ export const SideMenu = () => {
                     />
                 </ListItem>
 
-                <ListItem button>
+                <ListItemButton>
                     <ListItemIcon>
                         <AccountCircleOutlined/>
                     </ListItemIcon>
                     <ListItemText primary={'Perfil'} />
-                </ListItem>
+                </ListItemButton>
 
-                <ListItem button>
+                <ListItemButton>
                     <ListItemIcon>
                         <ConfirmationNumberOutlined/>
                     </ListItemIcon>
                     <ListItemText primary={'Mis Ordenes'} />
-                </ListItem>
+                </ListItemButton>
 
 
-                <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
+                <ListItemButton onClick={ () => navigateTo('/category/men') } 
+                    sx={{ display: { xs: '', sm: 'none' } }}
+                >
                     <ListItemIcon>
                         <MaleOutlined/>
                     </ListItemIcon>
                     <ListItemText primary={'Hombres'} />
-                </ListItem>
+                </ListItemButton>
 
-                <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
+                <ListItemButton onClick={ () => navigateTo('/category/women') } 
+                    sx={{ display: { xs: '', sm: 'none' } }}
+                >
                     <ListItemIcon>
                         <FemaleOutlined/>
                     </ListItemIcon>
                     <ListItemText primary={'Mujeres'} />
-                </ListItem>
+                </ListItemButton>
 
-                <ListItem button sx={{ display: { xs: '', sm: 'none' } }}>
+                <ListItemButton onClick={ () => navigateTo('/category/kid') } 
+                    sx={{ display: { xs: '', sm: 'none' } }}
+                >
                     <ListItemIcon>
                         <EscalatorWarningOutlined/>
                     </ListItemIcon>
                     <ListItemText primary={'Niños'} />
-                </ListItem>
+                </ListItemButton>
 
 
-                <ListItem button>
+                <ListItemButton>
                     <ListItemIcon>
                         <VpnKeyOutlined/>
                     </ListItemIcon>
                     <ListItemText primary={'Ingresar'} />
-                </ListItem>
+                </ListItemButton>
 
-                <ListItem button>
+                <ListItemButton>
                     <ListItemIcon>
                         <LoginOutlined/>
                     </ListItemIcon>
                     <ListItemText primary={'Salir'} />
-                </ListItem>
+                </ListItemButton>
 
 
                 {/* Admin */}
                 <Divider />
                 <ListSubheader>Admin Panel</ListSubheader>
 
-                <ListItem button>
+                <ListItemButton>
                     <ListItemIcon>
                         <CategoryOutlined/>
                     </ListItemIcon>
                     <ListItemText primary={'Productos'} />
-                </ListItem>
-                <ListItem button>
+                </ListItemButton>
+                <ListItemButton>
                     <ListItemIcon>
                         <ConfirmationNumberOutlined/>
                     </ListItemIcon>
                     <ListItemText primary={'Ordenes'} />
-                </ListItem>
+                </ListItemButton>
 
-                <ListItem button>
+                <ListItemButton>
                     <ListItemIcon>
                         <AdminPanelSettings/>
                     </ListItemIcon>
                     <ListItemText primary={'Usuarios'} />
-                </ListItem>
+                </ListItemButton>
             </List>
         </Box>
     </Drawer>
