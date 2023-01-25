38ce708d9ed624182b4f76e5a11d70e484c212ec
Manuel Ulate <fr3zs09@gmail.com>
2023-01-24 20:31:47 -0600

    mostrar producto en carrito

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,28 +1,29 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 import NextLink from 'next/link'
 import { Box, Button, CardActionArea, CardMedia, Grid, Link, Typography } from "@mui/material";
 import { initialData } from "database/products"
 import { ItemCouter } from './';
-
-
-const productsInCart = [ initialData.products[0], initialData.products[1], initialData.products[2] ];
+import { CartContext } from '../../context/cart/CartContext';
 
 interface Props {
     editable?: boolean
 }
 
 export const CartList: FC<Props> = ({ editable = false }) => {
+
+    const { cart } = useContext( CartContext );
+
   return (
     <>
         {
-            productsInCart.map( ( p ) => (
+            cart.map( ( p ) => (
                 <Grid container spacing={ 2 } key={p.slug} sx={{ mb: 1 }}>
                     <Grid item xs={ 3 }>
                         <NextLink href="/product/slug" passHref legacyBehavior>
                             <Link>
                                 <CardActionArea>
                                     <CardMedia 
-                                        image={ `/products/${ p.images[0] }` }
+                                        image={ `/products/${ p.image }` }
                                         component='img'
                                         alt={ p.title }
                                         sx={{ borderRadius: '5px' }}
@@ -33,18 +34,18 @@ export const CartList: FC<Props> = ({ editable = false }) => {
                     </Grid>
                     <Grid item xs={ 7 }>
                         <Box display='flex' flexDirection='column'>
-                            <Typography variant='body1'>{p.title}</Typography>
-                            <Typography variant='body1'>Talla: <strong>M</strong></Typography> 
+                            <Typography variant='body1'>{ p.title }</Typography>
+                            <Typography variant='body1'>Talla: <strong>{ p.size }</strong></Typography> 
                         </Box>
                         {
                             editable
-                            ? <ItemCouter />
-                            : <Typography variant='h5' component='h5'>3 items</Typography>
+                            ? <ItemCouter  currentValue={ p.quantity }  maxValue={ 10 }  updatedQuantity={ () => {} }/>
+                            : <Typography variant='h5' component='h5'>{ p.quantity } { p.quantity > 1 ? 'productos' : 'producto' }</Typography>
                              
                         }
                     </Grid>
                     <Grid item xs={ 2 } display='flex' alignItems='center' flexDirection='column'>
-                        <Typography variant='subtitle1' fontWeight={400}>${p.price}</Typography>
+                        <Typography variant='subtitle1' fontWeight={400}>${ p.price }</Typography>
                         {
                             editable && (
                                 <Button variant='text' color='secondary'>
