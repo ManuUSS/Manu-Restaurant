3ca42ffd1a9220c5cee427d6c7fc597556687b22
Manuel Ulate <fr3zs09@gmail.com>
2023-01-26 21:07:36 -0600

    proveedor de credenciales

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,29 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+    
+    CredentialsProvider({
+        name: 'Custom login',
+        credentials: {
+            email: { label: 'Correo', type: 'email', placeholder: 'correo@dominio.com' },
+            password: { label: 'Contraseña', type: 'password', placeholder: 'Contraseña' },
+        },
+        async authorize ( credentials ) {
+            return null;
+        }
     }),
-    // ...add more providers here
+    GithubProvider({
+        clientId: process.env.GITHUB_ID!,
+        clientSecret: process.env.GITHUB_SECRET!,
+      }),
   ],
+  callbacks: {
+    
+  }
 }
 
 export default NextAuth(authOptions);
