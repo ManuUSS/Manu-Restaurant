6729ca233afa174985b5020515f9727223a3fe35
Manuel Ulate <fr3zs09@gmail.com>
2023-01-27 14:53:21 -0600

    fin modelo orden

--- models/Order.ts
+++ models/Order.ts
@@ -3,8 +3,32 @@ import { IOrder } from 'interfaces';
 
 
 const orderSchema = new Schema({
-    
-    name: { type: String, required: true }
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    orderItems: [{
+        _id     : { type: Schema.Types.ObjectId, ref: 'Product', required: true },     
+        title   : { type: String, required: true },   
+        size    : { type: String, required: true },    
+        quantity: { type: Number, required: true },
+        slug    : { type: String, required: true },    
+        image   : { type: String, required: true },   
+        price   : { type: Number, required: true }   
+    }],
+    shippingAddress: {
+        firstName   : { type: String, required: true },
+        lastName    : { type: String, required: true },
+        address     : { type: String, required: true },
+        address2    : { type: String  },
+        zip         : { type: String, required: true },
+        city        : { type: String, required: true },
+        country     : { type: String, required: true },
+        phone       : { type: String, required: true },
+    },
+    numberOfItems   : { type: Number, require: true }, 
+    subTotal        : { type: Number, require: true }, 
+    tax             : { type: Number, require: true }, 
+    total           : { type: Number, require: true }, 
+    isPaid          : { type: Boolean, require: true, default: false }, 
+    paidAt          : { type: String }
     
 }, {
     timestamps: true
--- models/index.ts
+++ models/index.ts
@@ -1,3 +1,4 @@
 
 export { default as Product } from './Products';
 export { default as User } from './User';
+export { default as Order } from './Order';
