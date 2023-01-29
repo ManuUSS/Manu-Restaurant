126eb0bf008af2f02e4c505ee7b3b0f39a75e7d1
Manuel Ulate <fr3zs09@gmail.com>
2023-01-29 13:58:10 -0600

    creacion de orden en backend

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,8 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react';
+import { IOrder } from '../../../interfaces/order';
+import { db } from 'database';
+import { Order, Product } from 'models';
 
-type Data = {
-    message: string
-}
+type Data = 
+| { message: string }
+| IOrder
 
 export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
 
@@ -18,9 +22,50 @@ export default function handler (req: NextApiRequest, res: NextApiResponse<Data>
     res.status(200).json({ message: 'Example' });
 }
 
-const createOrder = ( req: NextApiRequest, res: NextApiResponse<Data> ) => {
+const createOrder = async ( req: NextApiRequest, res: NextApiResponse<Data> ) => {
 
-    const { body } = req;
+    const { orderItems, total } = req.body as IOrder;
 
-    res.status(201).json( body );
+    const session: any = await getSession({ req });
+
+    if( !session ) {
+        return res.status(401).json({ message: 'No tienes autorizacion para realizar este procedimiento' });
+    }
+
+    const productsIds = orderItems.map( ( p ) => p._id );
+    await db.connect();
+    
+    const dbProducts = await Product.find({ _id: { $in: productsIds } });
+    
+    try {
+        const subTotal =  orderItems.reduce( ( prev, current ) => {
+
+            const currentPrice = dbProducts.find( prod => prod.id === current._id )?.price;
+            if( !currentPrice ) {
+                throw new Error('Verifique el carrito de nuevo, producto no existe');
+            }
+
+
+            return ( currentPrice * current.quantity ) + prev 
+        
+        }, 0 );
+
+        const taxRate = Number( process.env.NEXT_PUBLIC_TAX_RATE || 0 );
+        const backendTotal = subTotal * ( taxRate + 1 );
+
+        if( total !== backendTotal ) {
+            throw new Error('El total no cuadra con el monto');
+        }
+
+        const userId = session.user._id; 
+        const newOrder = new Order({ ...req.body, isPaid: false, user: userId });
+        await newOrder.save();
+
+        return res.status(201).json( newOrder );
+
+    } catch ( error ) {
+        await db.disconnect();
+        return res.status(400).json({ message: 'Revisar los logs del servidor' });
+    }
+
 }   
