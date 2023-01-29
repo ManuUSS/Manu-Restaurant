ce2e0861dd71ed38dbba4ae80492be5b3ceae419
Manuel Ulate <fr3zs09@gmail.com>
2023-01-29 15:25:54 -0600

    historial de ordenes

--- pages/orders/history.tsx
+++ pages/orders/history.tsx
@@ -1,8 +1,12 @@
 import NextLink from 'next/link';
+import { GetServerSideProps, NextPage } from 'next';
 import { ShopLayout } from 'components/layout';
 import { Typography, Grid, Chip, Link } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid/models';
+import { getSession } from 'next-auth/react';
+import { dbOrders } from 'database';
+import { IOrder } from '../../interfaces/order';
 
 const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID', width: 100 },
@@ -31,7 +35,7 @@ const columns: GridColDef[] = [
         renderCell: ( params:GridRenderCellParams ) => {
             
             return (
-                <NextLink href={`/orders/${params.row.id}`} passHref legacyBehavior>
+                <NextLink href={`/orders/${params.row.orderId}`} passHref legacyBehavior>
                     <Link underline='always'>
                         Ver orden
                     </Link>
@@ -42,20 +46,24 @@ const columns: GridColDef[] = [
     }
 ] 
 
-const rows = [
-    { id: 1, paid: false , fullName: 'Manuel Ulate Sancho' },
-    { id: 2, paid: true , fullName: 'Leo Chaves Barrientos' },
-    { id: 3, paid: false , fullName: 'Joshua Villegas Bolaños' },
-    { id: 4, paid: true , fullName: 'Juan Peréz Jacitno' },
-    { id: 5, paid: false , fullName: 'Seanecio Herrera' },
-]
+interface Props {
+    orders: IOrder[]
+}
 
-const HistoryPage = () => {
+const HistoryPage: NextPage<Props> = ({ orders }) => {
+
+    const rows = orders.map( ( order, index ) => ({
+        id: index + 1,  
+        paid: order.isPaid , 
+        fullName: `${ order.shippingAddress.firstName } ${ order.shippingAddress.lastName }`,
+        orderId: order._id
+    }))
+
   return (
     <ShopLayout title='Historial de Ordenes' pageDescription='Historial de ordenes del cliente'>
         <>
             <Typography variant='h1' component='h1'>Historial de órdenes</Typography>
-            <Grid container>
+            <Grid container className='fadeIn'>
                 <Grid item xs={12} sx={{ height: 650, width: '100%' }}>
                     <DataGrid 
                         rows={ rows }
@@ -68,6 +76,28 @@ const HistoryPage = () => {
         </>
     </ShopLayout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    
+    const session:any = await getSession({ req });
+
+    if( !session ) {
+        return{
+            redirect: {
+                destination: '/auth/login?p=/orders/history',
+                permanent: false
+            }
+        }
+    }
+
+    const orders = await dbOrders.getOrdersByUser( session.user._id );
+
+    return {
+        props: {
+            orders       
+        }
+    }
 }
 
 export default HistoryPage
