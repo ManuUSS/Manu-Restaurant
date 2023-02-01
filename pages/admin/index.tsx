619d82c74f244aff49a6ea8a48ea65fcef5035b0
Manuel Ulate <fr3zs09@gmail.com>
2023-02-01 12:09:19 -0600

    datos en el dashboard desde el api

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,50 +1,84 @@
 import { AttachMoneyOutlined, CreditCardOutlined, DashboardOutlined, GroupOutlined, CategoryOutlined, CancelPresentationOutlined, ProductionQuantityLimitsOutlined, AccessTimeOutlined } from '@mui/icons-material';
-import { Card, CardContent, Grid, Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import { AdminLayout } from 'components/layout/AdminLayout'
 import React from 'react'
 import { SummaryTitle } from '../../components/admin/SummaryTitle';
+import useSwr from 'swr';
+import { DashboardSummaryResponse } from 'interfaces';
+import { useState, useEffect } from 'react';
 
 const DashboardPage = () => {
-  return (
+  
+    const { data, error } = useSwr<DashboardSummaryResponse>('/api/admin/dashboard', {
+        refreshInterval: 30000
+    });
+
+    const [ refreshIn, setRefreshIn ] = useState( 30 );
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setRefreshIn( refreshIn => refreshIn > 0 ? refreshIn - 1: 30 );
+      }, 1000)
+    
+      return () => {
+        clearInterval( interval );
+      }
+    }, []);
+    
+
+    if( !error && !data ) {
+        return <></>
+    }
+
+    if( error ) {
+        console.log( error );
+        return <Typography>Error al cargar la información</Typography>
+    } 
+    
+    const { 
+        numberOfOrders,
+        paidOrders,
+        numberOfClients,
+        numberOfProducts,
+        productsWithNoInventory,
+        lowInventory,
+        notPaidOrders 
+    } = data!;
+    return (
     <AdminLayout title='Dasboard' subTitle='Estadisticas generales' icon={ <DashboardOutlined /> }>
         <Grid container spacing={ 2 }>
             <SummaryTitle 
-                title={ 1 }
+                title={ numberOfOrders }
                 subTitle="Ordenes totales"
                 icon={ <CreditCardOutlined color='secondary' sx={{ fontSize: 40 }} /> } 
             />
             <SummaryTitle 
-                title={ 1 }
+                title={ paidOrders }
                 subTitle="Ordenes pagadas"
                 icon={ <AttachMoneyOutlined color='success' sx={{ fontSize: 40 }} /> } 
             />
             <SummaryTitle 
-                title={ 1 }
+                title={ notPaidOrders }
                 subTitle="Ordenes pendientes"
                 icon={ <CreditCardOutlined color='error' sx={{ fontSize: 40 }} /> } 
             />
             <SummaryTitle 
-                title={ 1 }
+                title={ numberOfClients }
                 subTitle="Clientes"
                 icon={ <GroupOutlined color='primary' sx={{ fontSize: 40 }} /> } 
             />
             <SummaryTitle 
-                title={ 1 }
+                title={ numberOfProducts }
                 subTitle="Productos"
                 icon={ <CategoryOutlined color='warning' sx={{ fontSize: 40 }} /> } 
             />
             <SummaryTitle 
-                title={ 1 }
-                subTitle="Ordenes totales"
-                icon={ <CreditCardOutlined color='secondary' sx={{ fontSize: 40 }} /> } 
-            />
-            <SummaryTitle 
-                title={ 1 }
+                title={ productsWithNoInventory }
                 subTitle="Sin existencias"
                 icon={ <CancelPresentationOutlined color='error' sx={{ fontSize: 40 }} /> } 
             />
             <SummaryTitle 
-                title={ 1 }
+                title={ lowInventory }
                 subTitle="Bajo inventario"
                 icon={ <ProductionQuantityLimitsOutlined color='warning' sx={{ fontSize: 40 }} /> } 
             />
